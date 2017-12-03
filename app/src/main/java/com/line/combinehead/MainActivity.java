aae684c7d44c2ed2990ebc9850b2cce96a6da50f
chenliu <1101587382@qq.com>
2017-12-03 18:19:09 +0800

    update

--- app/src/main/java/com/line/combinehead/MainActivity.java
+++ app/src/main/java/com/line/combinehead/MainActivity.java
@@ -1,13 +1,76 @@
 package com.line.combinehead;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.line.combinehead.combinehead.CombineHeadUtil;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Queue;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    static Queue<String> urls = new LinkedList<>();
+    static {
+        urls.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1512702265&di=37b926d77df562f97e26f1598a6e0d92&imgtype=jpg&er=1&src=http%3A%2F%2Fpic27.photophoto.cn%2F20130601%2F0036036307762415_b.jpg");
+        urls.add("https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=272162387,1882373772&fm=27&gp=0.jpg");
+        urls.add("https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1841735587,1754178892&fm=27&gp=0.jpg");
+        urls.add("https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=4077282133,9308138&fm=27&gp=0.jpg");
+        urls.add("https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3141321719,4227905030&fm=27&gp=0.jpg");
+        urls.add("https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3265039697,1790980665&fm=27&gp=0.jpg");
+        urls.add("https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=4042920819,4259410653&fm=27&gp=0.jpg");
+        urls.add("https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=4249499471,3103145698&fm=27&gp=0.jpg");
+        urls.add("https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2846910142,166539610&fm=27&gp=0.jpg");
+    }
+
+    private List<Bitmap> bitmapList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.image);
+        bitmapList = new ArrayList<>();
+        imageView.post(new Runnable() {
+            @Override
+            public void run() {
+                final int imageSize = imageView.getWidth();
+                loadBitmap(new Observer() {
+                    @Override
+                    public void update(Observable o, Object arg) {
+                        Bitmap head = CombineHeadUtil.getConbineHead(bitmapList, imageSize, 10, Color.LTGRAY);
+                        imageView.setImageBitmap(head);
+                    }
+                });
+            }
+        });
+
+
+    }
+
+    private void loadBitmap(final Observer observer) {
+        Glide.with(this).load(urls.poll()).asBitmap().override(100, 100).into(new SimpleTarget<Bitmap>() {
+            @Override
+            public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
+                bitmapList.add(resource);
+                if(urls.size() > 0){
+                    loadBitmap(observer);
+                } else {
+                    observer.update(null, null);
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/com/line/combinehead/combinehead/CombineHeadUtil.java
+++ app/src/main/java/com/line/combinehead/combinehead/CombineHeadUtil.java
@@ -2,7 +2,6 @@ package com.line.combinehead.combinehead;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Matrix;
 import android.support.annotation.ColorInt;
 
@@ -14,9 +13,9 @@ import java.util.List;
 
 public class CombineHeadUtil {
 
-    public static class LeftTop{
-        public float left;
-        public float top;
+    static class LeftTop{
+        float left;
+        float top;
     }
 
     /**
@@ -30,24 +29,24 @@ public class CombineHeadUtil {
     public static Bitmap getConbineHead(List<Bitmap> headBitmaps, int imageSize, int spaceing, @ColorInt int bgColor){
         if(headBitmaps == null || headBitmaps.size() == 0)  return null;
         int count = Math.min(headBitmaps.size(), 9);
-        int widthSize = mSize - edgeWidth * 2;
-        int heightSize = mSize - edgeWidth * 2;
-        Bitmap groupAvator = Bitmap.createBitmap(mSize, mSize, Bitmap.Config.ARGB_8888);
+        int itemwidth = getItemWidth(count, imageSize, spaceing);
+        Bitmap groupAvator = Bitmap.createBitmap(imageSize, imageSize, Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(groupAvator);
-        canvas.drawColor(Color.LTGRAY);
+        canvas.drawColor(bgColor);
+        LeftTop leftTop = new LeftTop();
         for (int i = 0; i < count; i++) {
-            Bitmap bitmap = data.get(i);
+            Bitmap bitmap = headBitmaps.get(i);
             int width = bitmap.getWidth();
             int height = bitmap.getHeight();
             Matrix matrix = new Matrix();
-            matrix.postScale(widthSize * 1.0f / width, heightSize * 1.0f / height);
+            matrix.postScale(itemwidth * 1.0f / width, itemwidth * 1.0f / height);
             Bitmap newBit = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, false);
-            canvas.drawBitmap(newBit, edgeWidth, edgeWidth, null);
+            calculateLeftTop(count, imageSize, itemwidth, spaceing, i, leftTop);
+            canvas.drawBitmap(newBit, leftTop.left, leftTop.top, null);
         }
         canvas.save();
         canvas.restore();
-
-        return null;
+        return groupAvator;
     }
 
     private static int getItemWidth(int count, int imageSize, int spaceing) {
@@ -95,50 +94,45 @@ public class CombineHeadUtil {
                     leftTop.top = (imageSize - 2 * itemWidth -  spaceing) / 2 + spaceing + itemWidth;
                 }
             break;
-            case 6: {
-                    float left = 0;
-                    float top = 0;
-                    if (i < 3) {
-                        top = (mSize - 2 * heightSize -  edgeWidth) / 2;
-                        left = i * widthSize + (i + 1 ) * edgeWidth;
+            case 6:
+                if (index < 3) {
+                    leftTop.left = index * itemWidth + (index + 1 ) * spaceing;
+                    leftTop.top = (imageSize - 2 * itemWidth -  spaceing) / 2;
+                } else {
+                    leftTop.left = (index - 3) * itemWidth + (index - 2) * spaceing;
+                    leftTop.top = (imageSize - 2 * itemWidth -  spaceing) / 2 + spaceing + itemWidth;
+                }
+                break;
+            case 7:
+                    if (index == 0) {
+                        leftTop.top = spaceing;
+                        leftTop.left = (imageSize - itemWidth) / 2;
                     } else {
-                        top = (mSize - 2 * heightSize -  edgeWidth) / 2 + edgeWidth + heightSize;
-                        left = (i - 3) * widthSize + (i - 2) * edgeWidth;
-                    }
-            }
-            case 7: {
-                    float left = 0;
-                    float top = 0;
-                    if (i == 0) {
-                        top = edgeWidth;
-                        left = (mSize - widthSize) / 2;
-                    } else {
-                        top = (i + 2) / 3 * heightSize + ((i + 2) / 3 + 1) * edgeWidth;
-                        if(i <= 3){
-                            left = (i - 1) * widthSize + i * edgeWidth;
+                        leftTop.top = (index + 2) / 3 * itemWidth + ((index + 2) / 3 + 1) * spaceing;
+                        if(index <= 3){
+                            leftTop.left = (index - 1) * itemWidth + index * spaceing;
                         } else {
-                            left = (i - 4) * widthSize + (i - 3) * edgeWidth;
+                            leftTop.left = (index - 4) * itemWidth + (index - 3) * spaceing;
                         }
                     }
-            }
-            case 8: {
-                    float left = 0;
-                    float top = 0;
-                    if (i < 2) {
-                        top = edgeWidth;
-                        left = (mSize - 2 * widthSize - edgeWidth) / 2 + i * widthSize + i * edgeWidth;
+            break;
+            case 8:
+                    if (index < 2) {
+                        leftTop.top = spaceing;
+                        leftTop.left = (imageSize - 2 * itemWidth - spaceing) / 2 + index * itemWidth + index * spaceing;
                     } else {
-                        top = (i + 3) / 4 * heightSize + ((i + 3 ) / 4 + 1) * edgeWidth;
-                        if(i <= 4){
-                            left = (i - 2) * widthSize + (i - 1) * edgeWidth;
+                        leftTop.top = (index + 3) / 4 * itemWidth + ((index + 3 ) / 4 + 1) * spaceing;
+                        if(index <= 4){
+                            leftTop.left = (index - 2) * itemWidth + (index - 1) * spaceing;
                         }else{
-                            left = (i - 5) * widthSize + (i - 4) * edgeWidth;
+                            leftTop.left = (index - 5) * itemWidth + (index - 4) * spaceing;
                         }
                     }
-            }
-            case 9: {
-                    float left = i % 3 * widthSize + (i % 3 + 1) * edgeWidth;
-                    float top = (i / 3) * heightSize + (i / 3 + 1) * edgeWidth;
+            break;
+            case 9:
+                    leftTop.left = index % 3 * itemWidth + (index % 3 + 1) * spaceing;
+                    leftTop.top = (index / 3) * itemWidth + (index / 3 + 1) * spaceing;
+                    break;
         }
     }
 
